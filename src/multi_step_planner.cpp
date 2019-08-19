f7d9336113da9255a0a94bc49db7a6e981a6d076
Ginny Xiao <ginnyxiao@berkeley.edu>
2019-08-20 07:05:01 +0800

    using multi-threaded spinning; receiving multiple goals while planning and publishing

--- src/multi_step_planner.cpp
+++ src/multi_step_planner.cpp
@@ -1,5 +1,7 @@
 #include <atomic>
 #include <ros/ros.h>
+#include <mutex>
+#include <thread>
 
 // #include <moveit/move_group_interface/move_group.h>
 #include <moveit/move_group_interface/move_group_interface.h>
@@ -13,18 +15,44 @@
 
 using moveit::planning_interface::MoveGroupInterface;
 
-// global variables
-ros::Publisher g_path_publisher;
-std::unique_ptr<tf::TransformBroadcaster> g_broadcaster;
-std::unique_ptr<tf::TransformListener> g_listener;
-const char *g_planning_frame = "odom_combined"; // TODO: take this from the MoveGroupInterface
-const char *g_robot_frame = "base_footprint";
-std::atomic<bool> g_move_group_busy(false);
-std::vector<geometry_msgs::PoseStamped> g_grasp_pose_buffer;
+// WaitForGoal -> Finished
+// WaitForGoal -> PlanToGoal
+// PlanToGoal -> PublishPath
+// PublishPath -> WaitForGoal
+
+enum struct PickState
+{
+    Finished = -1,
+    WaitForGoal = 0,
+    PlanToGoal,
+    PublishPath,
+    Count
+};
+
+struct PickMachine;
+
+struct PickMachState
+{
+    using EnterFn = void (*)(PickMachine* mach, PickState);
+    using PumpFn = PickState (*)(PickMachine* mach);
+    using ExitFn = void (*)(PickMachine* mach, PickState);
+
+    EnterFn enter = NULL;
+    PumpFn pump = NULL;
+    ExitFn exit = NULL;
+};
 
 struct PickMachine
 {
+    PickState prev_state;
+    PickState curr_state;
+    PickState next_state;
+
+    PickMachState* states;
+
 	std::unique_ptr<MoveGroupInterface> move_group;
+
+    std::atomic<bool> goal_ready;
 
 	std::vector<double> dropoff_position;
 	std::vector<double> home_position;
@@ -34,6 +62,32 @@ struct PickMachine
 
 	geometry_msgs::PoseStamped grasp_pose_goal;
 };
+
+struct WorldState
+{
+    std::mutex grasp_mutex;
+    std::vector<geometry_msgs::PoseStamped> grasp_pose_buffer;
+};
+
+// global variables
+ros::Publisher g_path_publisher;
+std::unique_ptr<tf::TransformBroadcaster> g_broadcaster;
+std::unique_ptr<tf::TransformListener> g_listener;
+const char *g_planning_frame = "odom_combined"; // TODO: take this from the MoveGroupInterface
+const char *g_robot_frame = "base_footprint";
+std::atomic<bool> g_move_group_busy(false);
+WorldState g_world_state;
+std::vector<geometry_msgs::PoseStamped> g_grasp_pose_buffer;
+
+void LockWorldState(WorldState* state)
+{
+    state->grasp_mutex.lock();
+}
+
+void UnlockWorldState(WorldState* state)
+{
+    state->grasp_mutex.unlock();
+}
 
 bool TryHardTransformVector(
     const std::string& frame_id,
@@ -74,28 +128,24 @@ void WaitForMoveGroup()
     while (g_move_group_busy) { ros::Duration(0.02).sleep(); }
 }
 
-void PublishPlannedPath(const MoveGroupInterface::Plan* plan, int rate = 10, int iterations = 1000)
-{
-    int iteration = 0; 
-    ros::Rate r(rate);
-    while (ros::ok && iteration < iterations) {
-        walker_simulation::Path1 path;
-        for (int i = 0; i < plan->trajectory_.joint_trajectory.points.size(); i++) {
-            walker_simulation::GraspPose robot_state;
-            for (int j = 0; j < plan->trajectory_.joint_trajectory.points[i].positions.size(); j++) {
-                robot_state.a.push_back(plan->trajectory_.joint_trajectory.points[i].positions[j]);
-            }
-            path.path.push_back(robot_state);
-        }
-        iteration++;
-        g_path_publisher.publish(path);
-        r.sleep();
-    }
-}
-
-void GraspPoseCallback(const geometry_msgs::PoseStamped grasp)
+void GraspPoseCallback(const geometry_msgs::PoseStamped& grasp)
 {
 	g_grasp_pose_buffer.push_back(grasp);
+    ROS_INFO("buffer size: %d", g_grasp_pose_buffer.size());
+
+}
+
+PickState DoWaitForGoal(PickMachine* mach)
+{
+    while (ros::ok()) {
+        if (mach->goal_ready) {
+            mach->goal_ready = false; // consume goal
+            return PickState::PlanToGoal;
+        }
+        ros::Duration(1.0).sleep();
+    }
+
+    return PickState::Finished;
 }
 
 bool PlanFromCurrentState(PickMachine *mach)
@@ -107,18 +157,24 @@ bool PlanFromCurrentState(PickMachine *mach)
     g_move_group_busy = false;
 
     if (err.val != moveit_msgs::MoveItErrorCodes::SUCCESS) {
-        ROS_ERROR("Failed to plan to grasp pose");
+        ROS_ERROR("Failed to plan to pose (from current robot state)!");
 		return false;
     }
-
-	ros::param::set("/walker_planner_done", 1);
-	mach->previous_plan = mach->current_plan;
 	return true;
 }
 
-bool GetLastRobotState(PickMachine *mach, moveit_msgs::RobotState& state)
+/* 
+struct Plan{
+	moveit_msgs::RobotState start_state_;
+	moveit_msgs::RobotTrajectory trajectory_;
+	double planning_time_;
+};*/
+
+void GetLastRobotState(PickMachine *mach, moveit_msgs::RobotState& state)
 {
-	return 1;
+	state.joint_state.name = mach->previous_plan.trajectory_.joint_trajectory.joint_names;
+	int joint_traj_len = mach->previous_plan.trajectory_.joint_trajectory.points.size();
+	state.joint_state.position = mach->previous_plan.trajectory_.joint_trajectory.points[joint_traj_len-1].positions;
 }
 
 bool PlanFromLastState(PickMachine *mach)
@@ -133,25 +189,49 @@ bool PlanFromLastState(PickMachine *mach)
     g_move_group_busy = false;
 
     if (err.val != moveit_msgs::MoveItErrorCodes::SUCCESS) {
-        ROS_ERROR("Failed to plan to grasp pose");
-		return 0;
+        ROS_ERROR("Failed to plan to pose (from last planned state)! ");
+		return false;
     }
-
-	ros::param::set("/walker_planner_done", 1);
-	mach->previous_plan = mach->current_plan;
-	return 1;
+	return true;
 }
 
-void Plan(PickMachine* mach)
+PickState DoPlanToGoal(PickMachine* mach)
 {
-	ros::param::set("/walker_planner_request", 0);
-	WaitForMoveGroup();
 	bool plan_succeed;
 	plan_succeed = PlanFromCurrentState(mach);
-	if (plan_succeed)
-	{
-		PublishPlannedPath(&mach->current_plan);
+
+    g_grasp_pose_buffer.erase(g_grasp_pose_buffer.begin());
+	ros::param::set("/walker_planner_done", 1);
+
+	if (plan_succeed) {
+        ROS_WARN("Successfully planned to goal pose!");
+        mach->previous_plan = mach->current_plan;
+		return PickState::PublishPath;
+	} else {
+		mach->current_plan = mach->previous_plan;
+        return PickState::WaitForGoal;
 	}
+}
+
+PickState DoPublishPath(PickMachine* mach)
+{
+    int iteration = 0; 
+    int iterations = 1000;
+    ros::Rate r(10);
+    while (ros::ok && iteration < iterations) {
+        walker_simulation::Path1 path;
+        for (int i = 0; i < mach->current_plan.trajectory_.joint_trajectory.points.size(); i++) {
+            walker_simulation::GraspPose robot_state;
+            for (int j = 0; j < mach->current_plan.trajectory_.joint_trajectory.points[i].positions.size(); j++) {
+                robot_state.a.push_back(mach->current_plan.trajectory_.joint_trajectory.points[i].positions[j]);
+            }
+            path.path.push_back(robot_state);
+        }
+        iteration++;
+        g_path_publisher.publish(path);
+        r.sleep();
+    }
+    return PickState::WaitForGoal;
 }
 
 auto MakeConveyorCollisionObject() -> moveit_msgs::CollisionObject
@@ -201,47 +281,130 @@ auto MakeConveyorCollisionObject() -> moveit_msgs::CollisionObject
     return conveyor;
 }
 
+auto to_cstring(PickState state) -> const char*
+{
+    switch (state) {
+    case PickState::WaitForGoal:        return "WaitForGoal";
+    case PickState::PlanToGoal:         return "PlanToGoal";
+    case PickState::PublishPath:        return "PublishPath";
+    default:                            return "<Unknown>";
+    }
+};
+
+void RunStateMachine(PickMachine* mach)
+{
+    while (ros::ok()) {
+        if (mach->prev_state != mach->curr_state) {
+            ROS_INFO("Enter state '%s' -> '%s'", to_cstring(mach->prev_state), to_cstring(mach->curr_state));
+            if (mach->states[(int)mach->curr_state].enter) {
+                mach->states[(int)mach->curr_state].enter(mach, mach->prev_state);
+            }
+            mach->prev_state = mach->curr_state;
+        }
+
+        // assert(mach->states[(int)mach->curr_state] != NULL);
+        mach->next_state = mach->states[(int)mach->curr_state].pump(mach);
+        if (mach->next_state == PickState::Finished) {
+            break;
+        }
+
+        if (mach->next_state != mach->curr_state) {
+            ROS_INFO("Exit state '%s' -> '%s'", to_cstring(mach->curr_state), to_cstring(mach->next_state));
+            if (mach->states[(int)mach->curr_state].exit) {
+                mach->states[(int)mach->curr_state].exit(mach, mach->curr_state);
+            }
+            mach->curr_state = mach->next_state;
+        }
+    }
+}
+
+bool PlanForNextGoal(PickMachine* mach)
+{
+    ROS_INFO("Trying to plan to next goal...");
+    
+    if (g_grasp_pose_buffer.empty()) {
+		ROS_WARN("No grasp goals available!");
+		return false;
+	}
+
+    if (mach->goal_ready) return false; // we're busy
+
+    int req;
+    ros::param::get("/walker_planner_request", req);
+    if (req)
+    {
+        ROS_INFO("Planning request received!");
+        ros::param::set("/walker_planner_request", 0);
+        WaitForMoveGroup();
+        mach->grasp_pose_goal = g_grasp_pose_buffer[0];
+        mach->goal_ready = true;
+        return true;
+    }
+    return false;
+}
+
 
 int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "multi_step_planner");
 	ros::NodeHandle nh;
 	ros::NodeHandle ph("~");
-	ros::Rate loop_rate(10);
+
+    ros::AsyncSpinner spinner(2);
+    spinner.start();
 
 	double allowed_planning_time;
 	ph.param("allowed_planning_time", allowed_planning_time, 3.0);
+    ros::param::set("/walker_planner_request", 0);
+    ros::param::set("/walker_planner_done", 1);
+    ros::param::set("/from_current_state", 1);
 
 	g_broadcaster.reset(new tf::TransformBroadcaster);
     g_listener.reset(new tf::TransformListener);
 
-	ros::Subscriber pose_sub = nh.subscribe("/Grasps", 1000, GraspPoseCallback);
+	ros::Subscriber pose_sub = nh.subscribe("/Grasp", 1000, GraspPoseCallback);
 	g_path_publisher = nh.advertise<walker_simulation::Path1>("Robot_path", 1000);
+
+    // WaitForGoal -> Finished
+    // WaitForGoal -> PlanToGoal
+    // PlanToGoal -> PublishPath
+    // PublishPath -> WaitForGoal
+
+    PickMachState states[(int)PickState::Count];
+    states[(int)PickState::WaitForGoal].pump = DoWaitForGoal;
+    states[(int)PickState::PlanToGoal].pump = DoPlanToGoal;
+    states[(int)PickState::PublishPath].pump = DoPublishPath;
 
 	ROS_INFO("Initialize right picking machine");
     PickMachine right_machine;
+    right_machine.prev_state = PickState::WaitForGoal;
+    right_machine.curr_state = PickState::WaitForGoal;
+    right_machine.next_state = PickState::WaitForGoal;
+    right_machine.goal_ready = false;
     right_machine.move_group.reset(new MoveGroupInterface(
                 "right_arm", boost::shared_ptr<tf::Transformer>(), ros::WallDuration(25.0)));
     right_machine.move_group->setPlanningTime(allowed_planning_time);
     right_machine.move_group->setPlannerId("right_arm[arastar_bfs_manip]");
     right_machine.move_group->setWorkspace(-0.4, -1.2, 0.0, 1.10, 1.2, 2.0);
     right_machine.move_group->startStateMonitor();
+    right_machine.states = states;
 
-	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-	planning_scene_interface.applyCollisionObject(MakeConveyorCollisionObject());
+	// moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+	// planning_scene_interface.applyCollisionObject(MakeConveyorCollisionObject());
 
+    auto r_mach_thread = std::thread(RunStateMachine, &right_machine);
+
+    ros::Rate loop_rate(10);
 	while (ros::ok())
 	{
-		int req;
-    	ros::param::get("/walker_planner_request", req);
-		if (req)
-		{
-			Plan(&right_machine);
-		}
-
-		loop_rate.sleep();
-		ros::spinOnce();
+        if (right_machine.curr_state == PickState::WaitForGoal){
+            PlanForNextGoal(&right_machine);
+        }
+        loop_rate.sleep();
 	}
+
+    ros::waitForShutdown();
+    r_mach_thread.join();
 
 	return 0;
 }
