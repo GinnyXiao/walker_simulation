fc86b6745ee5bb51f934c79c663d20d6692d5ae8
Ginny Xiao <ginnyxiao@berkeley.edu>
2019-08-20 00:26:42 +0800

    Added multi-step planner logic

---
+++ src/multi_step_planner.cpp
@@ -0,0 +1,247 @@
+#include <atomic>
+#include <ros/ros.h>
+
+// #include <moveit/move_group_interface/move_group.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <walker_simulation/Path1.h>
+#include <walker_simulation/GraspPose.h>
+
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+using moveit::planning_interface::MoveGroupInterface;
+
+// global variables
+ros::Publisher g_path_publisher;
+std::unique_ptr<tf::TransformBroadcaster> g_broadcaster;
+std::unique_ptr<tf::TransformListener> g_listener;
+const char *g_planning_frame = "odom_combined"; // TODO: take this from the MoveGroupInterface
+const char *g_robot_frame = "base_footprint";
+std::atomic<bool> g_move_group_busy(false);
+std::vector<geometry_msgs::PoseStamped> g_grasp_pose_buffer;
+
+struct PickMachine
+{
+	std::unique_ptr<MoveGroupInterface> move_group;
+
+	std::vector<double> dropoff_position;
+	std::vector<double> home_position;
+
+	MoveGroupInterface::Plan previous_plan;
+	MoveGroupInterface::Plan current_plan;
+
+	geometry_msgs::PoseStamped grasp_pose_goal;
+};
+
+bool TryHardTransformVector(
+    const std::string& frame_id,
+    const geometry_msgs::Vector3Stamped& vector_in,
+    geometry_msgs::Vector3Stamped& vector_out)
+{
+    while (true) {
+        try {
+            if (!ros::ok()) return false;
+            g_listener->transformVector(frame_id, vector_in, vector_out);
+            return true;
+        } catch (...) {
+
+        }
+        ros::Duration(0.01).sleep();
+    }
+}
+
+bool TryHardTransformPose(
+    const std::string& frame_id,
+    const geometry_msgs::PoseStamped& pose_in,
+    geometry_msgs::PoseStamped& pose_out)
+{
+    while (true) {
+        try {
+            if (!ros::ok()) return false;
+            g_listener->transformPose(frame_id, pose_in, pose_out);
+            return true;
+        } catch (...) {
+            ROS_INFO("Transform not available from '%s' to '%s'", pose_in.header.frame_id.c_str(), frame_id.c_str());
+        }
+        ros::Duration(0.01).sleep();
+    }
+}
+
+void WaitForMoveGroup()
+{
+    while (g_move_group_busy) { ros::Duration(0.02).sleep(); }
+}
+
+void PublishPlannedPath(const MoveGroupInterface::Plan* plan, int rate = 10, int iterations = 1000)
+{
+    int iteration = 0; 
+    ros::Rate r(rate);
+    while (ros::ok && iteration < iterations) {
+        walker_simulation::Path1 path;
+        for (int i = 0; i < plan->trajectory_.joint_trajectory.points.size(); i++) {
+            walker_simulation::GraspPose robot_state;
+            for (int j = 0; j < plan->trajectory_.joint_trajectory.points[i].positions.size(); j++) {
+                robot_state.a.push_back(plan->trajectory_.joint_trajectory.points[i].positions[j]);
+            }
+            path.path.push_back(robot_state);
+        }
+        iteration++;
+        g_path_publisher.publish(path);
+        r.sleep();
+    }
+}
+
+void GraspPoseCallback(const geometry_msgs::PoseStamped grasp)
+{
+	g_grasp_pose_buffer.push_back(grasp);
+}
+
+bool PlanFromCurrentState(PickMachine *mach)
+{
+	g_move_group_busy = true;
+	mach->move_group->setStartStateToCurrentState();
+    mach->move_group->setPoseTarget(mach->grasp_pose_goal.pose);
+    auto err = mach->move_group->plan(mach->current_plan);
+    g_move_group_busy = false;
+
+    if (err.val != moveit_msgs::MoveItErrorCodes::SUCCESS) {
+        ROS_ERROR("Failed to plan to grasp pose");
+		return false;
+    }
+
+	ros::param::set("/walker_planner_done", 1);
+	mach->previous_plan = mach->current_plan;
+	return true;
+}
+
+bool GetLastRobotState(PickMachine *mach, moveit_msgs::RobotState& state)
+{
+	return 1;
+}
+
+bool PlanFromLastState(PickMachine *mach)
+{
+	moveit_msgs::RobotState last_robot_state;
+	GetLastRobotState(mach, last_robot_state);
+
+	g_move_group_busy = true;
+	mach->move_group->setStartState(last_robot_state);
+    mach->move_group->setPoseTarget(mach->grasp_pose_goal.pose);
+    auto err = mach->move_group->plan(mach->current_plan);
+    g_move_group_busy = false;
+
+    if (err.val != moveit_msgs::MoveItErrorCodes::SUCCESS) {
+        ROS_ERROR("Failed to plan to grasp pose");
+		return 0;
+    }
+
+	ros::param::set("/walker_planner_done", 1);
+	mach->previous_plan = mach->current_plan;
+	return 1;
+}
+
+void Plan(PickMachine* mach)
+{
+	ros::param::set("/walker_planner_request", 0);
+	WaitForMoveGroup();
+	bool plan_succeed;
+	plan_succeed = PlanFromCurrentState(mach);
+	if (plan_succeed)
+	{
+		PublishPlannedPath(&mach->current_plan);
+	}
+}
+
+auto MakeConveyorCollisionObject() -> moveit_msgs::CollisionObject
+{
+     moveit_msgs::CollisionObject conveyor;
+
+    double height = 0.70; //0.64 + ADJUST;
+
+    geometry_msgs::PoseStamped p;
+    p.header.frame_id = g_robot_frame;
+    p.header.stamp = ros::Time(0); //ros::Time::now();
+    p.pose.position.x = 0.25;
+    p.pose.position.y = 0.0;
+    p.pose.position.z = 0.5 * height;
+    p.pose.orientation.w = 1.0;
+
+    geometry_msgs::PoseStamped p_out;
+    ROS_INFO("Transform conveyor object to planning frame");
+    TryHardTransformPose(g_planning_frame, p, p_out);
+    ROS_INFO("...done");
+
+    conveyor.header.frame_id = g_planning_frame;
+    conveyor.header.stamp = ros::Time::now();
+
+    conveyor.id = "conveyor";
+
+    shape_msgs::SolidPrimitive conveyor_shape;
+    conveyor_shape.type = shape_msgs::SolidPrimitive::BOX;
+    conveyor_shape.dimensions.resize(3);
+    conveyor_shape.dimensions[shape_msgs::SolidPrimitive::BOX_X] = 0.26;
+    conveyor_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 2.14;
+    conveyor_shape.dimensions[shape_msgs::SolidPrimitive::BOX_Z] = height;
+    conveyor.primitives.push_back(conveyor_shape);
+
+    geometry_msgs::Pose conveyor_pose;
+    conveyor_pose.position.x = p_out.pose.position.x;
+    conveyor_pose.position.y = p_out.pose.position.y;
+    conveyor_pose.position.z = p_out.pose.position.z;
+    conveyor_pose.orientation.w = p_out.pose.orientation.w;
+    conveyor_pose.orientation.x = p_out.pose.orientation.x;
+    conveyor_pose.orientation.y = p_out.pose.orientation.y;
+    conveyor_pose.orientation.z = p_out.pose.orientation.z;
+    conveyor.primitive_poses.push_back(conveyor_pose);
+
+    conveyor.operation = moveit_msgs::CollisionObject::ADD;
+
+    return conveyor;
+}
+
+
+int main(int argc, char *argv[])
+{
+	ros::init(argc, argv, "multi_step_planner");
+	ros::NodeHandle nh;
+	ros::NodeHandle ph("~");
+	ros::Rate loop_rate(10);
+
+	double allowed_planning_time;
+	ph.param("allowed_planning_time", allowed_planning_time, 3.0);
+
+	g_broadcaster.reset(new tf::TransformBroadcaster);
+    g_listener.reset(new tf::TransformListener);
+
+	ros::Subscriber pose_sub = nh.subscribe("/Grasps", 1000, GraspPoseCallback);
+	g_path_publisher = nh.advertise<walker_simulation::Path1>("Robot_path", 1000);
+
+	ROS_INFO("Initialize right picking machine");
+    PickMachine right_machine;
+    right_machine.move_group.reset(new MoveGroupInterface(
+                "right_arm", boost::shared_ptr<tf::Transformer>(), ros::WallDuration(25.0)));
+    right_machine.move_group->setPlanningTime(allowed_planning_time);
+    right_machine.move_group->setPlannerId("right_arm[arastar_bfs_manip]");
+    right_machine.move_group->setWorkspace(-0.4, -1.2, 0.0, 1.10, 1.2, 2.0);
+    right_machine.move_group->startStateMonitor();
+
+	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+	planning_scene_interface.applyCollisionObject(MakeConveyorCollisionObject());
+
+	while (ros::ok())
+	{
+		int req;
+    	ros::param::get("/walker_planner_request", req);
+		if (req)
+		{
+			Plan(&right_machine);
+		}
+
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
+
+	return 0;
+}
